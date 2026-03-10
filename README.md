9a5813b431faada70cdeaa6ab96fa3d27e0cd678
Sahil Joshi <sail.joshi0604@gmail.com>
2026-03-11 01:59:22 +0530

    model init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fraud Detection Classifier
+
+This project builds a Machine Learning model to predict whether there is a Fraud or not.
+
+## Dataset
+
+- Dataset used: Credit Card Fraud Detection
+
+Download it from : 
+[File Link](https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud)
+
+After downloading, place the files inside "dataset" folder.
+
+## Task
+
+- Handeled imbalanced dataset
+- RandomForestClassifier for prediction
+- GridSearchCV for hyperparameter tuning
+
+
+## Library Used
+- pandas
+- scikit-learn
---
+++ model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split , GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score , recall_score , precision_score
+
+df1 = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\Fraud Detection Model\creditcard.csv")
+
+x = df1.drop(["Class"] , axis=1)
+y = df1["Class"]
+
+xtrain , xtest , ytrain , ytest = train_test_split(x , y , test_size=0.2 , random_state=42 , stratify=y)
+
+
+param_grid = {"n_estimators" : [200,250,300]}
+
+grid = GridSearchCV(RandomForestClassifier(max_depth=None , class_weight="balanced") , param_grid , cv=5 , scoring="recall" , n_jobs=-1)
+
+grid.fit(xtrain , ytrain)
+
+model = grid.best_estimator_
+
+ypred = model.predict(xtest)
+print()
+
+# ---------------------
+# Evaluation Metric
+# ---------------------
+
+print(f"F1 Score : {f1_score(ytest , ypred)}")
+print(f"Recall : {recall_score(ytest , ypred)}")
+print(f"Precision : {precision_score(ytest , ypred)}")
